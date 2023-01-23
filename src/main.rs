4bccbdb9dcdc0e4b95699a24df4055e1c93fc629
Scott Lambert <s-lambert@live.com.au>
2023-01-23 22:59:06 +0800

    Add scoreboard to game

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,11 @@ const PADDLE_WIDTH: f32 = 38.0;
 
 const BLOCK_WIDTH: f32 = 20.0;
 
+#[derive(Resource)]
+struct Scoreboard {
+    score: i32,
+}
+
 #[derive(Component)]
 struct Velocity(Vec2);
 
@@ -22,6 +27,33 @@ struct Block;
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+fn setup_scoreboard(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(
+        TextBundle::from_sections([
+            TextSection::new(
+                "Score: ",
+                TextStyle {
+                    font: asset_server.load("pixel_font.ttf"),
+                    ..default()
+                },
+            ),
+            TextSection::from_style(TextStyle {
+                font: asset_server.load("pixel_font.ttf"),
+                ..default()
+            }),
+        ])
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                top: Val::Px(5.0),
+                left: Val::Px(5.0),
+                ..default()
+            },
+            ..default()
+        }),
+    );
 }
 
 fn setup_player(mut commands: Commands) {
@@ -142,12 +174,14 @@ fn ball_blocks_collision(
     ball_transform: Query<&Transform, With<Ball>>,
     mut ball_velocity: Query<&mut Velocity, With<Ball>>,
     block_transforms: Query<(Entity, &Transform), With<Block>>,
+    mut scoreboard: ResMut<Scoreboard>,
 ) {
     let Some(transform) = ball_transform.iter().next() else { return; };
     let Some(mut velocity) = ball_velocity.iter_mut().next() else { return; };
 
     for (block, block_transform) in block_transforms.iter() {
         if aabb(transform, block_transform) {
+            scoreboard.score += 1;
             commands.entity(block).despawn();
 
             let x_diff = (transform.translation.x - block_transform.translation.x).abs() / 15.0;
@@ -185,6 +219,11 @@ fn aabb(a: &Transform, b: &Transform) -> bool {
     return collision_x && collision_y;
 }
 
+fn update_scoreboard(scoreboard: Res<Scoreboard>, mut query: Query<&mut Text>) {
+    let mut text = query.single_mut();
+    text.sections[1].value = scoreboard.score.to_string();
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -197,7 +236,9 @@ fn main() {
             ..default()
         }))
         .insert_resource(ClearColor(Color::rgb(0.04, 0.0, 0.0)))
+        .insert_resource(Scoreboard { score: 0 })
         .add_startup_system(setup_camera)
+        .add_startup_system(setup_scoreboard)
         .add_startup_system(setup_player)
         .add_startup_system(setup_ball)
         .add_startup_system(setup_blocks)
@@ -207,5 +248,6 @@ fn main() {
         .add_system(ball_bounds_collision.after(ball_movement))
         .add_system(ball_player_collision.after(ball_bounds_collision))
         .add_system(ball_blocks_collision.after(ball_player_collision))
+        .add_system(update_scoreboard)
         .run();
 }
